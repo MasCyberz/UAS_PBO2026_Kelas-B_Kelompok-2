e80e662f848f7c10e61c247e2988ebdd83900bbe
ameliapinasti38-prog <ameliapinasti38@student.uns.ac.id>
2026-06-29 09:32:15 +0700

    Update pengembangan.py

---
+++ interfaces/pengembangan.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+class IPengembangan(ABC):
+    #Interface untuk standarisasi proses pengembangan (proofing) adonan.
+    
+    @abstractmethod
+    def kembangkan_adonan(self, durasi_menit: int) -> None:
+    #Metode untuk mensimulasikan proses pengembangan adonan.
+        pass
